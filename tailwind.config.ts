0e19ba5a3560f8307693646b50d75018a2134da6
davetwo7 <davidolu2019@gmail.com>
2024-01-31 19:44:22 -0500

    Initial setup with circle of fifths wheel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        'satoshi': ['Satoshi-Variable', 'sans-serif'], // Assuming you want to use the variable font for all weights.
+      },
+      fontWeight: {
+        'light': "300",
+        'regular': "400",
+        'medium': "500",
+        'bold': "700",
+        'black': "900",
+      },
     },
   },
   plugins: [],
