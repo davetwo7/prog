81acd888c87e9fe427457eac80f6b7e5aef9fec4
davetwo7 <davidolu2019@gmail.com>
2024-01-31 20:51:59 -0500

    Update SVG

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
+import CircleOfFifths from "./components/CircleOfFifths";
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col">
-        <div className=" bg-zinc-900 p-5 font-satoshi font-medium text-3xl">
+        <div className="  p-5 font-satoshi font-medium text-3xl">
           Circle of Fifths
         </div>
-        <div className="">
-          <Image
-            src="/test-wheel-circle-of-fifths.svg"
+        <div>Select a key:</div>
+        <div className="flex justify-center mt-5">
+          <CircleOfFifths />
+          {/* <Image
+            src="/circle-of-fifths.svg"
             alt="Description"
             width={500}
             height={500}
-          />
+          /> */}
         </div>
       </div>
     </main>
